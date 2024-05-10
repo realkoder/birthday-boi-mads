9693414d8c5015cc8d03d44e7029544ef6c49631
realkoder <alex8970@stud.kea.dk>
2024-05-11 01:41:53 +0200

    More cleaning for the view and more smooth mobile

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
     let counter = 0;
     setInterval(() => {
       setTextContent(
-        counter % 2 === 0 ? "" : "🇩🇰TILLYKKE MED DIN FØDSELSDAG!🇩🇰"
+        counter % 2 === 0 ? "🇩🇰ÅHHHHH FØDSELSDAG🇩🇰" : "🇩🇰TILLYKKE MED DIN FØDSELSDAG!🇩🇰"
       );
       setEvenFlag(counter % 2 === 0 ? "🇩🇰" : "🎁");
       setOddFlag(counter % 2 === 0 ? "🎁" : "🇩🇰");
@@ -24,36 +24,52 @@ export default function Home() {
   return (
     <main className="min-w-max min-h-max bg-green-100 flex">
       <div className="flex-1 flex flex-col text-center items-center">
-        <p className="text-xl m-12">{evenFlag}</p>
-        <p className="text-xl m-12">{oddFlag}</p>
-        <p className="text-xl m-12">{evenFlag}</p>
-        <p className="text-xl m-12">{oddFlag}</p>
+        <p className="text-lg my-4">{evenFlag}</p>
+        <p className="text-lg my-4">{oddFlag}</p>
+        <p className="text-lg my-4">{evenFlag}</p>
+        <p className="text-lg my-4">{oddFlag}</p>
+        <p className="text-lg my-4">{evenFlag}</p>
+        <p className="text-lg my-4">{oddFlag}</p>
+        <p className="text-lg my-4">{evenFlag}</p>
+        <p className="text-lg my-4">{oddFlag}</p>
+        <p className="text-lg my-4">{evenFlag}</p>
+        <p className="text-lg my-4">{oddFlag}</p>
+        <p className="text-lg my-4">{evenFlag}</p>
+        <p className="text-lg my-4">{oddFlag}</p>
       </div>
       <div className="flex-1 flex flex-col text-center items-center">
-        <h1 className="text-2xl m-16">ÅHHH FØDSELSDAGS MADS!!</h1>
-        <h1 className="text-xl m-8">
+        <h1 className="text-lg my-4">ÅHHH FØDSELSDAGS MADS!!</h1>
+        <h1 className="text-lg my-4">
           TILLYKKE MED DIN FØDSELSDAG BIRTHDAY BOI MADS
         </h1>
         <DialogBox />
-        <h2 className="text-xl m-8">{textContent}</h2>
-        <h2 className="text-xl m-8">{textContent}</h2>
-        <h2 className="text-xl m-8">{textContent}</h2>
-        <h2 className="text-xl m-8">{textContent}</h2>
+        <h2 className="text-lg my-4">{textContent}</h2>
+        <h2 className="text-lg my-4">{textContent}</h2>
+        <h2 className="text-lg my-4">{textContent}</h2>
+        <h2 className="text-lg my-4">{textContent}</h2>
         <iframe
           className="rounded-lg"
           frameborder="0"
-          height="315"
+          height="200"
           src="https://www.youtube.com/embed/Ifj_-a3l8P0?autoplay=1"
-          width="420"
+          width="200"
           allow="autoplay; fullscreen"
           allowfullscreen
         ></iframe>
       </div>
       <div className="flex-1 flex flex-col text-center items-center">
-        <p className="text-xl m-12">{evenFlag}</p>
-        <p className="text-xl m-12">{oddFlag}</p>
-        <p className="text-xl m-12">{evenFlag}</p>
-        <p className="text-xl m-12">{oddFlag}</p>
+      <p className="text-lg my-4">{evenFlag}</p>
+        <p className="text-lg my-4">{oddFlag}</p>
+        <p className="text-lg my-4">{evenFlag}</p>
+        <p className="text-lg my-4">{oddFlag}</p>
+        <p className="text-lg my-4">{evenFlag}</p>
+        <p className="text-lg my-4">{oddFlag}</p>
+        <p className="text-lg my-4">{evenFlag}</p>
+        <p className="text-lg my-4">{oddFlag}</p>
+        <p className="text-lg my-4">{evenFlag}</p>
+        <p className="text-lg my-4">{oddFlag}</p>
+        <p className="text-lg my-4">{evenFlag}</p>
+        <p className="text-lg my-4">{oddFlag}</p>
       </div>
     </main>
   );
